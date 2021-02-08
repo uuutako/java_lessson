579c5c8d1495308e942ea11eca5855862f0cc5e3
uuutako <okn.sk009@gmail.com>
2021-02-08 12:44:15 +0900

    Initial commit

---
+++ MyApp.java
@@ -0,0 +1,52 @@
+public class MyApp {
+
+  public static String sayHi(String name){
+    
+  }
+
+  public static void main(String[] args) {
+    
+    String msg =sayHi("Steve");
+    System.out.println(msg);
+  }
+
+
+}
+
+    // int []sales = {700,400,500};
+
+    // for (int sale : sales) {
+    //   System.out.println(sale);
+    // }
+    
+  //   for(int i = 0; i <sales.length; i++){
+  //       System.out.println(sales[i]);
+  //   }
+  // }
+
+    // double d =52343.23;
+    // int i = (int)d;
+    // System.out.println(i);
+  // //文字
+  // char a = 'a';
+  // //整数
+  // int x = 10;
+  // long y = 555555555L;
+  // //浮動小数点数　float double
+  // double d = 23423.344;
+  // float f =32.33F;
+  // //論理値
+  // boolean flag = true; //false
+  //文字列
+  //＼n 改行
+  // \t タブ
+
+  // //文字列
+  // String msg;
+  // msg = "Hello Wo\nrld Again\t!";
+
+  // String msg2 = "Hello World!";
+  
+  // System.out.println(msg);
+  // System.out.println(msg2);
+
